6b5898a331a2d433fb60cf29c609749369b5217e
Suvendra <suvendrac@yahoo.com>
2026-03-10 21:25:02 -0400

    Context Manager example added

--- agentic/ag_openai/contextmanager.py
+++ agentic/ag_openai/contextmanager.py
@@ -3,6 +3,11 @@ We will build a Travel Recommendation System that provides personalized travel s
 preferences and current location. The system will utilize a Context Manager to manage user preferences and 
 current context, allowing it to generate tailored travel recommendations.
 """
+import os
+import asyncio
+from openai import AsyncOpenAI
+from agents import Agent, RunContextWrapper, OpenAIChatCompletionsModel, Runner, function_tool, set_tracing_disabled
+from dotenv import load_dotenv
 from dataclasses import dataclass
 
 @dataclass
@@ -15,9 +20,8 @@ class UserPreference:
 @dataclass
 class UserContext:
     user_id: int
-    current_location: str
 
-class DummyUserPerferenceManager:
+class DummyUserPreferenceManager:
     def __init__(self):
         self.user_profs = [
             UserPreference(user_id=1, name="Alice", favorite_sports="Tennis", favorite_food="Pizza"),
@@ -27,17 +31,52 @@ class DummyUserPerferenceManager:
             UserPreference(user_id=5, name="Eve", favorite_sports="Rugby", favorite_food="Pasta")
     ]
 
-    def get_user_preference(self, user_id: int):
+    def get_user_preference(self, user_id: int) -> str:
         up = next((prof for prof in self.user_profs if prof.user_id == user_id), None)
         if up:
+            print(f"Fetched user preferences for user_id {user_id}: {up}")
             return f"User {up.name} prefers {up.favorite_sports} in sports, and in food prefers {up.favorite_food}."
         else:
             return "User not found."
 
 class ContextManager:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, model, ep, key):
+        set_tracing_disabled(True)
+        agent = AsyncOpenAI(base_url=ep, api_key=key)
+        agent_model = OpenAIChatCompletionsModel(model=model, openai_client=agent)
+
+        self.user_preference_agent = Agent[UserContext](
+            name="Travel Manager Agent",
+            instructions="You are a helpful assistant who provides tailored travel preferences for the user.",
+            model=agent_model,
+            tools=[ContextManager.get_user_preferences]
+        )
+
+    @function_tool
+    @staticmethod
+    def get_user_preferences(wrapper:RunContextWrapper[UserContext]) -> str:
+        """
+        This function simulates fetching user preferences.
+        """
+        dummy_manager = DummyUserPreferenceManager()
+        return dummy_manager.get_user_preference(wrapper.context.user_id)
+    
+    async def run(self):
+        user_info = UserContext(user_id=3)
+        location = "Italy"
+        result = await Runner.run(  
+            starting_agent=self.user_preference_agent,
+            input=f"Based on the user's food and sports preferences, can you provide a travel recommendation in {location}?",
+            context=user_info        
+        )
+        print(result.final_output)
 
 if __name__ == "__main__":
-    du = DummyUserPerferenceManager()
-    print(du.get_user_preference(2))
+    load_dotenv()
+
+    OLLAMA_MODEL = "llama3.1:8b"
+    OLLAMA_EP = "http://localhost:11434/v1"
+    OLLAMA_KEY = os.getenv('OLLAMA_API_KEY')
+
+    ta = ContextManager(model=OLLAMA_MODEL, ep=OLLAMA_EP, key=OLLAMA_KEY)
+    asyncio.run(ta.run())
